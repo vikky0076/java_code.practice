84cc36195b18738d09a180300742ab799afc17ab
VIGNESH <vikkyvikky132007@gmail.com>
2026-02-05 23:45:49 +0530

    Add files via upload

---
+++ types.java
@@ -0,0 +1,10 @@
+class types{
+    public static void main(String args[])
+        {
+        
+        String name = "vikky";
+        int age = 18;
+        System.out.println(name);
+        System.out.println(age);
+        } 
+}
